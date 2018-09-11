6dccdaedd36c19ae6d60266de6e42fc314f4fb5b
Sergey <snugryumov@gmail.com>
2018-09-11 19:40:48 +0300

    services desktop

--- js/script.js
+++ js/script.js
@@ -103,9 +103,9 @@ function openService (evt, servicesName) {
 
   var tablinks = document.getElementsByClassName("tablinks");
   for (var i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" tablinks--active", "");
   }
 
   document.getElementById(servicesName).style.display = "block";
-  evt.currentTarget.className += " active"
+  evt.currentTarget.className += " tablinks--active"
 }
